4ec9ed74876b277109d38025a534c6786c2dfcc8
rimo802020 <reem.elmorsy@gmail.com>
2021-03-09 09:53:17 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
 module.exports = {
   mode: 'development',
   entry: './src/client/index.js',
@@ -10,5 +12,12 @@ module.exports = {
       exclude: /node_modules/,
       loader: "babel-loader"
     }]
-  }
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    })
+  ]
 }
