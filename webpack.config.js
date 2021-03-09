b316062dc478db440b0b7c25990bcb9d20415e70
rimo802020 <reem.elmorsy@gmail.com>
2021-03-09 16:10:53 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: './src/client/index.js',
@@ -17,6 +19,20 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
-    })
-  ]
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+})
+  ],
+  devServer: {
+    port: 1234,
+    contentBase: path.resolve(__dirname, "dist"),
+    open: true
+  }
 }
